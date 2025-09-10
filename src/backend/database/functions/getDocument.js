307fa776ff6e8c50480ca373cc0da36cb6b1609a
Kaspar Bergert <kasparbergert6@gmail.com>
2025-09-10 20:54:33 +0300

    fix: edited getDocument.js for better integration and added new argument.

--- src/backend/database/functions/getDocument.js
+++ src/backend/database/functions/getDocument.js
@@ -1,15 +1,29 @@
 import isMongooseModel from "./helpers/isMongooseModel.js";
 
-export default async function getDocument(model, filter) {
-  /// Type checks
-
-  if (typeof filter !== "object" || filter === null) {
-    throw "getDocument(model, filter): call does not have valid type for filter parameter";
+/**
+ * 
+ * @param {Mongoose Model} model - mongoose model to search
+ * @param {Object} filter - filter mongoose uses to find the document
+ * @param {Object} contents - (optional) what to receive from the document
+ * @returns found list of data matching the filter
+ * @throws when function is used wrong.
+ */
+export default async function getDocument(model, filter, contents = {}) {
+  
+  // Type checks
+  if (typeof filter !== 'object' || filter === null) {
+    throw new Error('getDocument(model, filter, contents): invalid type for filter parameter');
   }
 
   if (!isMongooseModel(model)) {
-    throw "getDocument(model, filter): call does not have valid model parameter";
+    throw new Error('getDocument(model, filter, contents): invalid model parameter');
   }
 
-  return await model.find(filter).select().result; // return selection
+  if(typeof contents != "object" || Array.isArray(contents)){
+    throw new Error("getDocument(model, filter, contents): invalid contents parameter")
+  }
+  ///
+
+
+  return await model.find(filter).select(contents); // returns list because using .find()
 }
