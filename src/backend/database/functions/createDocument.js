f7bd5a8cf41bb6609741d91aa3413a404d8c72e4
Kaspar Bergert <kasparbergert6@gmail.com>
2025-09-10 20:53:16 +0300

    fix: createDocument.js throws "new Error()" instead of string to be have better integration.

--- src/backend/database/functions/createDocument.js
+++ src/backend/database/functions/createDocument.js
@@ -4,16 +4,16 @@ import isMongooseModel from "./helpers/isMongooseModel.js";
  * @param {MongooseModel} model - Mongoose model to create document in
  * @param {Object} data - Data to create document with. Data must have at least 1 field to be created.
  * @returns {Object} - Created document
- * @throws {Object} - returns error string if there was an error creating the document
+ * @throws - when function is used wrong
  */
 export default async function createDocument(model, data) {
   if (!isMongooseModel(model)) {
-    throw "createDocument(model, data): call does not have valid type for model parameter";
+    throw new Error("createDocument(model, data): call does not have valid type for model parameter");
   }
 
   //check if foods is an array and has at least one element
   if (typeof data !== "object") {
-    throw "createDocument(model, data): call does not have valid type for data parameter";
+    throw new Error("createDocument(model, data): call does not have valid type for data parameter");
   }
 
   //create day document
