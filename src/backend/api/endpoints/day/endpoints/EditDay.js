eec1ab8a1ffbd4cd4b32bc889aaae6076e5ab766
Kaspar Bergert <kasparbergert6@gmail.com>
2025-09-10 20:50:50 +0300

    feat: created EditDay.js endpoint

--- src/backend/api/endpoints/day/endpoints/EditDay.js
+++ src/backend/api/endpoints/day/endpoints/EditDay.js
@@ -1,6 +1,26 @@
 import getDocument from "../../../../database/functions/getDocument.js";
 import Days from "../../../../database/models/days.js";
 
-export default function EditDay(req, res) {
-  // document exists?
+export default async function EditDay(req, res) {
+  
+  const id = req.query['day_id'];
+  const newFoods = req.body["foods"]; 
+  
+  if(!Array.isArray(newFoods)){
+    return res.status(400).send({error: "new foods are not formatted in to a list"});
+  }
+
+  const document = await Days.findById(id);
+  if(!document){
+    return res.status(400).send({error: `day with '${id}' does not exist`});
+  }
+
+  document.foods = newFoods;  
+  try{
+    await document.save();
+  }catch(err){
+    return res.status(400).send({error: "array has food id/s that do not exist on the database."});
+  }
+  
+  return res.status(204).send();
 }
