a14122d23eeabe7a5e4de1d86468cd30ff2cd48f
Kaspar Bergert <kasparbergert6@gmail.com>
2025-09-10 20:46:08 +0300

    refactor: reduced GetDay.js file complexity

--- src/backend/api/endpoints/day/endpoints/GetDay.js
+++ src/backend/api/endpoints/day/endpoints/GetDay.js
@@ -2,29 +2,22 @@ import createDocument from "../../../../database/functions/createDocument.js";
 import getDocument from "../../../../database/functions/getDocument.js";
 import Days from "../../../../database/models/days.js";
 
+
+/*
+  to create new day, just call it with any non-valid id.
+  for example: nr 1 will always create a new day,
+*/
 export default async function GetDay(req, res) {
   const id = req.query["day_id"];
-
-  if (!id) {
+  
+  if(!id){
     return res
-      .status(400)
-      .send({ error: "Invalid or missing day_id", result: null });
+    .status(400)
+    .send({ error: "Invalid or missing day_id" });
   }
-
-  // document exists?
-  let day = await getDocument(Days, { _id: id }).catch((err) => {
-    return res.status(500).send({ error: err, result: null });
-  });
-
-  if (!day) {
-    //create day
-    day = await createDocument(Days, { date: new Date(), foods: [] }).catch(
-      (err) => {
-        return res.status(500).send({ error: err, result: null });
-      }
-    );
-    return res.status(201).send({ error: null, result: day });
-  }
-
-  return res.status(200).send({ error: null, result: day });
+      
+  //get document (or create one) and send it back
+  const document = await getDocument(Days, { _id: id });    
+  const day = !document ? await createDocument(Days, { date: new Date(), foods: [] }) : document;  
+  return res.status(200 + (day ? 0:1)).send({ result: day });
 }
